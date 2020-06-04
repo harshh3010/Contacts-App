2615a4887eaf6b13c8c46e5ea9e1461a3c2dadcf
Harsh Gyanchandani <harsh.gyanchandani@gmail.com>
2020-06-04 17:40:58 +0530

    Added circular reveal animation

--- app/src/main/java/com/codebee/contactsapp/AddContactActivity.java
+++ app/src/main/java/com/codebee/contactsapp/AddContactActivity.java
@@ -1,16 +1,25 @@
 package com.codebee.contactsapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
 public class AddContactActivity extends AppCompatActivity {
 
+    RevealAnimation mRevealAnimation;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_contact);
+
+        Intent intent = this.getIntent();
+
+        ConstraintLayout constraintLayout = (ConstraintLayout) findViewById(R.id.add_contact_constraint_layout);
+        mRevealAnimation = new RevealAnimation(constraintLayout, intent, this);
 
         exit();
     }
@@ -19,8 +28,14 @@ public class AddContactActivity extends AppCompatActivity {
         findViewById(R.id.add_contact_back_image).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                finish();
+                mRevealAnimation.unRevealActivity();
             }
         });
+    }
+
+    @Override
+    public void onBackPressed()
+    {
+        mRevealAnimation.unRevealActivity();
     }
 }
--- app/src/main/java/com/codebee/contactsapp/MainActivity.java
+++ app/src/main/java/com/codebee/contactsapp/MainActivity.java
@@ -1,6 +1,7 @@
 package com.codebee.contactsapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -20,8 +21,25 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.contacts_add_button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this,AddContactActivity.class));
+                startRevealActivity(v);
             }
         });
+    }
+
+    private void startRevealActivity(View v) {
+        //calculates the center of the View v you are passing
+        int revealX = (int) (v.getX() + v.getWidth() / 2);
+        int revealY = (int) (v.getY() + v.getHeight() / 2);
+
+        //create an intent, that launches the second activity and pass the x and y coordinates
+        Intent intent = new Intent(this, AddContactActivity.class);
+        intent.putExtra(RevealAnimation.EXTRA_CIRCULAR_REVEAL_X, revealX);
+        intent.putExtra(RevealAnimation.EXTRA_CIRCULAR_REVEAL_Y, revealY);
+
+        //just start the activity as an shared transition, but set the options bundle to null
+        ActivityCompat.startActivity(this, intent, null);
+
+        //to prevent strange behaviours override the pending transitions
+        overridePendingTransition(0, 0);
     }
 }
